9890f05ecec7ea898a862ca11ce88dfd91eb3293
Mustafa Kemal <mustafakcakaloglumain@outlook.com>
2023-12-18 13:56:26 +0300

    blabla

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,10 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-
   constructor() {}
-
+  products_jeans = [
+    {
+      name: 'Right Wing Jeans',
+      price: '$29.99',
+      description: 'lorem ipsum',
+      source: 'https://ionicframework.com/docs/img/demos/card-media.png',
+    },
+    {
+      name: 'Right Wing Jeans Pro ',
+      price: '$39.99',
+      description: 'lorem ipsum pro',
+      source: 'https://ionicframework.com/docs/img/demos/card-media.png',
+    },
+  ];
 }
