c1bb60b7e0437d9ad0482c59db080a4b25309d30
Mustafa Kemal <mustafakcakaloglumain@outlook.com>
2023-12-18 10:43:46 +0300

    product list task

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  Email_Login: string = '';
+  Password_Login: string = '';
 
   constructor() {}
 
+  print_email_login() {
+    console.log('Giriş Yapıldı! EMail:' + this.Email_Login);
+  }
+  print_password_login() {
+    console.log('Giriş Yapıldı! Şifre: ' + this.Password_Login);
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  Email_Signup: string = '';
+  Password_Signup: string = '';
+  Password_Signup_Confirm: string = '';
 
   constructor() {}
 
+  print_password_signup() {
+    console.log('Üye Olundu! Şifre: ' + this.Password_Signup);
+  }
+  print_email_signup() {
+    console.log('Üye Olundu!EMail: ' + this.Email_Signup);
+  }
 }
